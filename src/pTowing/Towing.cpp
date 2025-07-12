41ed31913db86cc498c3e582238206f19acbed93
Tom M. <thoma904@mit.edu>
2025-07-12 12:58:57 -0400

    Breadcrumb pTowing working baseline

--- src/pTowing/Towing.cpp
+++ src/pTowing/Towing.cpp
@@ -9,6 +9,7 @@
 #include "MBUtils.h"
 #include "ACTable.h"
 #include "Towing.h"
+#include <AngleUtils.h>
 
 using namespace std;
 
@@ -17,6 +18,15 @@ using namespace std;
 
 Towing::Towing()
 {
+  m_nav_x = 0;
+  m_nav_y = 0;
+  m_nav_heading = 0;
+  m_towed_x = 0;
+  m_towed_y = 0;
+  m_towing_position.clear();
+  m_start_x = 0;
+  m_start_y = 0;
+  m_cable_length = 10; // default value
 }
 
 //---------------------------------------------------------
@@ -48,8 +58,14 @@ bool Towing::OnNewMail(MOOSMSG_LIST &NewMail)
     bool   mstr  = msg.IsString();
 #endif
 
-     if(key == "FOO") 
-       cout << "great!";
+     if(key == "NAV_X") 
+       m_nav_x = msg.GetDouble();
+
+     else if(key == "NAV_Y") 
+       m_nav_y = msg.GetDouble();
+
+     else if(key == "NAV_HEADING") 
+       m_nav_heading = msg.GetDouble();
 
      else if(key != "APPCAST_REQ") // handled by AppCastingMOOSApp
        reportRunWarning("Unhandled Mail: " + key);
@@ -74,7 +90,77 @@ bool Towing::OnConnectToServer()
 bool Towing::Iterate()
 {
   AppCastingMOOSApp::Iterate();
-  // Do your thing here!
+
+  if(m_towing_position.size() == 1) //set starting position to act as deployment point
+  {
+    m_start_x = m_nav_x;
+    m_start_y = m_nav_y;
+  } 
+
+  // Store towing vessel position
+  m_towing_position.add_vertex(m_nav_x, m_nav_y);
+
+  // Trim list to keep it manageable, arbitrarily set to 500 points
+  if(m_towing_position.size() > 500)
+    m_towing_position.delete_vertex(0);
+
+  // Compute distance from starting point
+  double dx0 = m_nav_x - m_start_x;
+  double dy0 = m_nav_y - m_start_y;
+  double dist_from_start = hypot(dx0, dy0);
+
+  // define local variables
+  double dist = 0;
+  double towed_x = m_nav_x;
+  double towed_y = m_nav_y;
+
+  // find the point that is m_cable_length away from the start point 
+  for(int i = m_towing_position.size()-1; i > 0; i--) 
+  {
+    double x1 = m_towing_position.get_vx(i);
+    double y1 = m_towing_position.get_vy(i);
+    double x0 = m_towing_position.get_vx(i-1);
+    double y0 = m_towing_position.get_vy(i-1);
+
+    double seg_len = hypot(x1 - x0, y1 - y0);
+    dist += seg_len;
+
+    if(dist >= m_cable_length) // alittle clunky but it works
+    {
+      towed_x = x0;
+      towed_y = y0;
+      break;
+    }
+  }
+
+  if (dist_from_start < m_cable_length) // keeps towed body at start point until cable is fully deployed
+  {
+    towed_x = m_start_x;
+    towed_y = m_start_y;
+  }
+
+  else // the towed body is moving so update its position
+  {
+    m_towed_x = towed_x;
+    m_towed_y = towed_y;
+  }
+
+  // Publish towing position
+  string tb_pos_str = "x=" + doubleToStringX(m_towed_x,1) + ",y=" +
+                    doubleToStringX(m_towed_y,1);
+  Notify("TOWING_POSITION", tb_pos_str);
+
+  string tb_hdg_str = "heading=" + doubleToStringX(m_nav_heading,1);
+  Notify("TOWING_HEADING", tb_hdg_str);
+
+  // Publish towed body for visualization
+  string body_str = "x=" + doubleToStringX(m_towed_x,1) + ",y=" +
+                    doubleToStringX(m_towed_y,1) + ",label=TOW_BODY";
+  body_str += ",type=diamond,color=red";
+  body_str += ",heading=" + doubleToStringX(m_nav_heading,1);
+  Notify("VIEW_POINT", body_str);
+
+
   AppCastingMOOSApp::PostReport();
   return(true);
 }
@@ -86,6 +172,8 @@ bool Towing::Iterate()
 bool Towing::OnStartUp()
 {
   AppCastingMOOSApp::OnStartUp();
+
+  m_cable_length = 10; // default value
 
   STRING_LIST sParams;
   m_MissionReader.EnableVerbatimQuoting(false);
@@ -100,10 +188,9 @@ bool Towing::OnStartUp()
     string value = line;
 
     bool handled = false;
-    if(param == "foo") {
-      handled = true;
-    }
-    else if(param == "bar") {
+    if(param == "cable_length") 
+    {
+      m_cable_length = stod(value);
       handled = true;
     }
 
@@ -122,7 +209,9 @@ bool Towing::OnStartUp()
 void Towing::registerVariables()
 {
   AppCastingMOOSApp::RegisterVariables();
-  // Register("FOOBAR", 0);
+  Register("NAV_X", 0);
+  Register("NAV_Y", 0);
+  Register("NAV_HEADING", 0);
 }
 
 
@@ -132,14 +221,15 @@ void Towing::registerVariables()
 bool Towing::buildReport() 
 {
   m_msgs << "============================================" << endl;
-  m_msgs << "File:                                       " << endl;
+  m_msgs << "  Towing Simulation Status                  " << endl;
   m_msgs << "============================================" << endl;
 
-  ACTable actab(4);
-  actab << "Alpha | Bravo | Charlie | Delta";
-  actab.addHeaderLines();
-  actab << "one" << "two" << "three" << "four";
-  m_msgs << actab.getFormattedString();
+  m_msgs << " NAV_X: " << m_nav_x << endl;
+  m_msgs << " NAV_Y: " << m_nav_y << endl;
+  m_msgs << " HEADING: " << m_nav_heading << endl;
+  m_msgs << " TOWED_X: " << m_towed_x << endl;
+  m_msgs << " TOWED_Y: " << m_towed_y << endl;
+  m_msgs << " CABLE_LENGTH: " << m_cable_length << endl;
 
   return(true);
 }
--- src/pTowing/Towing.h
+++ src/pTowing/Towing.h
@@ -9,6 +9,7 @@
 #define Towing_HEADER
 
 #include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+#include "XYSegList.h"
 
 class Towing : public AppCastingMOOSApp
 {
@@ -31,6 +32,15 @@ class Towing : public AppCastingMOOSApp
  private: // Configuration variables
 
  private: // State variables
+ double m_nav_x;
+ double m_nav_y;
+ double m_nav_heading;
+ double m_towed_x;
+ double m_towed_y;
+ double m_cable_length;
+ XYSegList m_towing_position;
+ double m_start_x;
+ double m_start_y;
 };
 
 #endif 
