34748bfbd8a115a707829c02efbec88c103c08ea
Tom M. <thoma904@mit.edu>
2025-08-04 11:52:33 -0400

    Did not work, will delete branch later

--- src/pTowing2/Towing.cpp
+++ src/pTowing2/Towing.cpp
@@ -11,7 +11,6 @@
 #include "ACTable.h"
 #include "Towing.h"
 #include <AngleUtils.h>
-#include <Eigen/Dense>
 
 using namespace std;
 
@@ -32,6 +31,22 @@ Towing::Towing()
   m_prev_heading = 0;
   m_nav_speed = 0;
   m_prev_time = MOOSTime();
+  m_cable_tension = 100; //Newtons
+  m_cable_angle = m_nav_heading; // initial angle same as heading
+  m_rho     = 1025.0;
+
+  m_m_t   = 10.0;     // strip added mass   [kg]
+  m_x_T   = -2.0;     // lever arm          [m]
+  m_m     = 50.0;     // dry mass           [kg]
+  m_M_35  = 0.0;
+  m_M_55  = 100.0;
+
+  m_ms_33 = 20.0;
+  m_ms_35 = 0.0;
+  m_ms_55 = 50.0;
+
+  m_u3 = m_omega2 = m_psi = 0.0;
+  m_b_pos.setZero();
 }
 
 //---------------------------------------------------------
@@ -74,6 +89,9 @@ bool Towing::OnNewMail(MOOSMSG_LIST &NewMail)
 
      else if(key == "NAV_SPEED")
        m_nav_speed = msg.GetDouble();
+
+     else if(key == "CABLE_TENSION")
+       m_cable_tension = msg.GetDouble();
 
      else if(key != "APPCAST_REQ") // handled by AppCastingMOOSApp
        reportRunWarning("Unhandled Mail: " + key);
@@ -122,6 +140,69 @@ bool Towing::Iterate()
   // R = [R1; R2]
   // Simplified M Matrix with Newman U3 and Omega2 initially 0 assumption:
   // M = [m^s_33 + m, 0; 0, m^s_55 + M_55]
+
+   //--------------------------- 1.  Cable unit vector & angle
+  Eigen::Vector2d tow_pos(m_nav_x, m_nav_y);          // tug position
+  Eigen::Vector2d r_vec = tow_pos - m_b_pos;          // body → tug
+  double r_norm = r_vec.norm();
+  Eigen::Vector2d r_hat =
+    (r_norm > 1e-6) ? r_vec.normalized()
+                    : Eigen::Vector2d(1, 0);
+
+  // body‑fixed components of r_hat
+  double hdg_body = MOOSDeg2Rad(m_nav_heading) + m_psi;   // world yaw
+  double cosH = cos(hdg_body);
+  double sinH = sin(hdg_body);
+
+  double x_b =  cosH * r_hat.x() + sinH * r_hat.y();      // surge
+  double y_b = -sinH * r_hat.x() + cosH * r_hat.y();      // sway (starboard+)
+
+  m_cable_angle = atan2(y_b, x_b);                        // diag only
+
+  //--------------------------- 2.  Lateral cable force  F3
+  const double F3 = m_cable_tension * y_b;                // [N]
+
+  //--------------------------- 3.  Assemble RHS (R1, R2)
+  const double U1 = m_nav_speed;      // surge speed [m/s]
+
+  const double R1 = -F3
+                    + U1 * m_m_t * m_u3
+                    - U1 * (m_x_T * m_m_t + m_m) * m_omega2;
+
+  const double R2 =  F3 * m_x_T
+                    - U1 * (m_ms_33 + m_x_T * m_m_t) * m_u3
+                    - U1 * (m_ms_35 + m_M_35 - m_x_T * m_x_T * m_m_t)
+                          * m_omega2;
+
+  //--------------------------- 4.  Solve  M · vdot = R
+  Eigen::Matrix2d M;
+  M << m_ms_33 + m_m,          m_ms_35 + m_M_35,
+       m_ms_35 + m_M_35,       m_ms_55 + m_M_55;
+
+  Eigen::Vector2d vdot = M.ldlt().solve(Eigen::Vector2d(R1, R2));
+  double Udot3     = -vdot(0);
+  double Omegadot2 = -vdot(1);
+
+  //--------------------------- 5.  Integrate body states
+  m_u3     += Udot3     * dt;
+  m_omega2 += Omegadot2 * dt;
+  m_psi    += m_omega2 * dt;
+
+  //--------------------------- 6.  World‑frame velocity & position
+  double Vx =  U1 * cosH - m_u3 * sinH;
+  double Vy =  U1 * sinH + m_u3 * cosH;
+
+  m_b_pos.x() += Vx * dt;
+  m_b_pos.y() += Vy * dt;
+
+  //--------------------------- 7.  MOOS outputs
+  Notify("TOWED_X",        m_b_pos.x());
+  Notify("TOWED_Y",        m_b_pos.y());
+  Notify("TOWED_HEADING",  MOOSRad2Deg(hdg_body));
+  Notify("TOWED_U3",       m_u3);
+  Notify("TOWED_R",        m_omega2);
+  Notify("CABLE_ANGLE",    MOOSRad2Deg(m_cable_angle));
+  Notify("F3_LATERAL",     F3);
 
   AppCastingMOOSApp::PostReport();
   return(true);
@@ -175,6 +256,7 @@ void Towing::registerVariables()
   Register("NAV_Y", 0);
   Register("NAV_HEADING", 0);
   Register("NAV_SPEED", 0);
+  Register("CABLE_TENSION", 0);
 }
 
 
--- src/pTowing2/Towing.h
+++ src/pTowing2/Towing.h
@@ -10,7 +10,7 @@
 
 #include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
 #include "XYSegList.h"
-
+#include <Eigen/Dense>
 
 class Towing : public AppCastingMOOSApp
 {
@@ -46,6 +46,23 @@ class Towing : public AppCastingMOOSApp
  double m_prev_heading;
  double m_nav_speed;
  double m_prev_time;
+ double m_cable_tension;
+ double m_cable_angle;
+ // Hydro Coef
+ double m_rho;               // water density                 [kg/m^3]
+ double m_delta_r;           // rudder angle (port +)         [rad] (updated in OnNewMail)
+
+ double m_m_t;               // added mass at stern           [kg]
+ double m_x_T;               // longitudinal ref. point       [m]  (usually negative)
+ double m_m;                 // dry mass                      [kg]
+ double m_M_35, m_M_55;      // dry cross prod. & yaw inertia [kg·m], [kg·m^2]
+ double m_ms_33, m_ms_35, m_ms_55;  // strip‑theory added‑mass terms
+
+ // Matrix Var
+ Eigen::Vector2d m_b_pos;    // world position  (x,y)         [m]
+ double          m_u3;       // sway velocity U3              [m/s]
+ double          m_omega2;   // yaw rate Ω2                   [rad/s]
+ double          m_psi;      // yaw angle (offset from NAV)   [rad]
 };
 
 #endif 
