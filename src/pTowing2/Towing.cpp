df15fd875f78715696f2da8a715e987f97e9b890
Tom M. <thoma904@mit.edu>
2025-08-04 12:13:08 -0400

    added visualization from mass spring model

--- src/pTowing2/Towing.cpp
+++ src/pTowing2/Towing.cpp
@@ -104,7 +104,16 @@ bool Towing::Iterate()
   m_prev_time = now;
   if(dt <= 0)    // safety
     return true;
-
+  
+  //Goal:
+  // Predict the XY position of the towed body when influenced by towing vehicle
+  //Description:
+  // Equations of motion from Newman's Marine Hydrodynamics (equations 60 and 61 from Chapter 7)
+  // initial assumptions:
+  // Symmetric Cylinder for Towed Body
+  // Towed Body is completely submerged
+  // Cable angle acts as rudder force
+  // U_3 and Omega_2 are initially 0
   //ODE's
   // F_3 = Tension*sin(cable_angle)
   // F_3 = U_1 * m_t * U_3 - U_1 * (x_T * m_T + m) * Omega_2 + (m^s_33 + m)*Udot_3 + (m^s_35 +M_35)*Omegadot_2
@@ -125,6 +134,33 @@ bool Towing::Iterate()
   // R = [R1; R2]
   // Simplified M Matrix with Newman U3 and Omega2 initially 0 assumption:
   // M = [m^s_33 + m, 0; 0, m^s_55 + M_55]
+
+
+  // -----------------------
+  // Publish position and visuals
+  // -----------------------
+  string tb_pos_str = "x=" + doubleToStringX(m_towed_x,1) + ",y=" +
+                      doubleToStringX(m_towed_y,1);
+  Notify("TOWING_POSITION", tb_pos_str);
+
+  string tb_hdg_str = "heading=" + doubleToStringX(tow_heading,1);
+  Notify("TOWING_HEADING", tb_hdg_str);
+
+  // VIEW_POINT for MarineViewer
+  string body_str = "x=" + doubleToStringX(m_towed_x,1) + ",y=" +
+                    doubleToStringX(m_towed_y,1) + ",label=TOW_BODY";
+  body_str += ",type=diamond,color=red";
+  body_str += ",heading=" + doubleToStringX(tow_heading,1);
+  Notify("VIEW_POINT", body_str);
+
+  // VIEW_SEGLIST for cable
+  string cable_str = "pts={" + doubleToStringX(m_nav_x,1) + "," +
+                              doubleToStringX(m_nav_y,1) + ":" +
+                              doubleToStringX(m_towed_x,1) + "," +
+                              doubleToStringX(m_towed_y,1) + "}";
+  cable_str += ",label=TOW_LINE";
+  cable_str += ",edge_color=gray,edge_size=2,vertex_size=0";
+  Notify("VIEW_SEGLIST", cable_str);
 
   AppCastingMOOSApp::PostReport();
   return(true);
