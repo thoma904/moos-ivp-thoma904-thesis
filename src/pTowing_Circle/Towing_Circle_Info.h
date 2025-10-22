71cc1ea801f871ba6d381e2e1cefc2fe3d037bb2
Tom M. <thoma904@mit.edu>
2025-10-22 15:56:43 -0400

    pTowing_Circle addition

---
+++ src/pTowing_Circle/Towing_Circle_Info.h
@@ -0,0 +1,18 @@
+/****************************************************************/
+/*   NAME: Tom Monaghan                                             */
+/*   ORGN: MIT, Cambridge MA                                    */
+/*   FILE: Towing_Circle_Info.h                                      */
+/*   DATE: Dececmber 29th, 1963                                 */
+/****************************************************************/
+
+#ifndef Towing_Circle_INFO_HEADER
+#define Towing_Circle_INFO_HEADER
+
+void showSynopsis();
+void showHelpAndExit();
+void showExampleConfigAndExit();
+void showInterfaceAndExit();
+void showReleaseInfoAndExit();
+
+#endif
+
