c5d867e8f4c17294cda9524b00d7e0a45362fadb
Tom M. <thoma904@mit.edu>
2025-10-23 12:00:20 -0400

    added ptowingobstaclemanager

---
+++ src/pTowObstacleMgr/TowObstacleMgr_Info.h
@@ -0,0 +1,18 @@
+/****************************************************************/
+/*   NAME: Tom Monaghan                                             */
+/*   ORGN: MIT, Cambridge MA                                    */
+/*   FILE: TowObstacleMgr_Info.h                                      */
+/*   DATE: Dececmber 29th, 1963                                 */
+/****************************************************************/
+
+#ifndef TowObstacleMgr_INFO_HEADER
+#define TowObstacleMgr_INFO_HEADER
+
+void showSynopsis();
+void showHelpAndExit();
+void showExampleConfigAndExit();
+void showInterfaceAndExit();
+void showReleaseInfoAndExit();
+
+#endif
+
